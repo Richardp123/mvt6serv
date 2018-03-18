64755b5349beff06568cedcd0270639648e038f4
gregsakai <gregsakai@gmail.com>
2018-03-18 01:10:14 -0700

    sticker object and stick function

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const port = process.env.PORT || 10000;
-const server= require("http").Server();
+const server = require("http").Server();
 
 var io = require("socket.io")(server);
 
@@ -7,46 +7,48 @@ var allusers = {};
 var allstickers = {};
 
 io.on("connection", function(socket){
-    // console.log("someone is connected");
-    // allusers.push(socket.id);
-
+    console.log("user is connected");
+	
     socket.on("stick", function(data){
-      allstickers[this.myRoom].push(data);
-      io.to(this.myRoom).emit("newsticker", allstickers[this.myRoom]);
+        allstickers[this.myRoom].push(data);
+        
+        io.to(this.myRoom).emit("newsticker", allstickers[this.myRoom]);
     });
-
+    
+    
     socket.on("joinroom", function(data){
-      console.log(data);
-      socket.join(data);
-
-      socket.myRoom(data);
-      socket.emit("yourid", socket.id);
-
-      if(!allusers[data]){
-        allusers[data] = [];
-      }
-      if(!allstickers[data]){
-        allstickers[data] = [];
-      }
-
-      allusers[data].push(socket.id);
-      io.to(data).emit("usersjoined", allusers[data]);
-
+        console.log(data);
+        socket.join(data);
+        
+        socket.myRoom = data;
+        socket.emit("yourid", socket.id);
+        
+        if(!allusers[data]){
+            allusers[data] = [];
+        }
+        
+        if(!allstickers[data]){
+            allstickers[data] = [];
+        }
+        
+        allusers[data].push(socket.id);
+        io.to(data).emit("userjoined", allusers[data]);
+        io.to(data).emit("newsticker", allstickers[data]);
+        
     });
-
+    
     socket.on("mymove", function(data){
-        // socket.broadcast.emit("usermove", data);
-        socket.to(this.myRoom).emit("newmove", data);
+        console.log(data);
+        socket.to(this.myRoom).emit("newmove", data); 
     });
-
+    
     socket.on("disconnect", function(){
-        // var index = allusers.indexOf(socket.id);
-        // allusers.splice(index, 1);
-        // io.emit("createimage", allusers);
-        var index = allusers[this.myRoom].indexOf(socket.id);
-        allusers[this.myRoom].splice(index,1);
-        io.to(this.myRoom).emit("usersjoined", allusers[this.myRoom]);
-    });
+        if(this.myRoom){
+            var index = allusers[this.myRoom].indexOf(socket.id);
+            allusers[this.myRoom].splice(index,1);
+            io.to(this.myRoom).emit("userjoined", allusers[this.myRoom]);
+        }
+    })
 });
 
 server.listen(port, (err)=>{
@@ -54,6 +56,6 @@ server.listen(port, (err)=>{
         console.log(err);
         return false;
     }
-
-    console.log("port is running");
+    
+    console.log("Port is running");
 })
