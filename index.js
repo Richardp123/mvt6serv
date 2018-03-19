ea863f6b31ecb4b8c379e0a49105990783be257d
gregsakai <gregsakai@gmail.com>
2018-03-19 15:06:03 -0700

    added chatroom sockets, BUG: chatroom disables sticker functions

--- index.js
+++ index.js
@@ -3,45 +3,59 @@ const server = require("http").Server();
 
 var io = require("socket.io")(server);
 
+var usernames = [];
+var msgs = [];
+
 var allusers = {};
 var allstickers = {};
 
 io.on("connection", function(socket){
     console.log("user is connected");
-	
+
+    socket.on("username", function(data){
+        console.log("user is giving username:"+data);
+        usernames.push(data);
+        
+        io.emit("usersjoined", usernames);
+
+    });
+
+    socket.on("sendChat", function(data){
+        console.log("user sent a msg for chat");
+        msgs.push(data);
+
+        io.emit('msgsent', msgs);
+    });
+
     socket.on("stick", function(data){
         allstickers[this.myRoom].push(data);
-        
         io.to(this.myRoom).emit("newsticker", allstickers[this.myRoom]);
     });
-    
-    
+
     socket.on("joinroom", function(data){
         console.log(data);
         socket.join(data);
-        
+
         socket.myRoom = data;
         socket.emit("yourid", socket.id);
-        
+
         if(!allusers[data]){
             allusers[data] = [];
         }
-        
+
         if(!allstickers[data]){
             allstickers[data] = [];
         }
-        
+
         allusers[data].push(socket.id);
         io.to(data).emit("userjoined", allusers[data]);
         io.to(data).emit("newsticker", allstickers[data]);
-        
     });
-    
+
     socket.on("mymove", function(data){
-        console.log(data);
-        socket.to(this.myRoom).emit("newmove", data); 
+        socket.to(this.myRoom).emit("newmove", data);
     });
-    
+
     socket.on("disconnect", function(){
         if(this.myRoom){
             var index = allusers[this.myRoom].indexOf(socket.id);
@@ -56,6 +70,6 @@ server.listen(port, (err)=>{
         console.log(err);
         return false;
     }
-    
+
     console.log("Port is running");
 })
